ddbd4612c7fd7b49c6a8458080af14d0999fe5d6
Adilhamidi <adil.hamidii123@gmail.com>
2026-05-28 17:16:44 +0200

    Initial Commit

---
+++ VirtualSIM/VirtualSIMApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualSIMApp.swift
+//  VirtualSIM
+//
+//  Created by Adyl on 28/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct VirtualSIMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
